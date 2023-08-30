62469c0bb24e5c599c2202de83420e8a66cbce18
holee <holee@ati2000.co.kr>
2023-08-30 14:47:35 +0900

    분산, 3시그마 계산추가 + 서브라인끼리 모음

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import glob
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.cm as cm
+from datetime import datetime as dt  # Rename the datetime module
 from xml.etree import ElementTree
 from xml.dom import minidom
 from PyQt5.QtCore import QObject, pyqtSignal
@@ -134,9 +136,9 @@ class MyClass:
         self.Ytick_TB.textChanged.connect(self.on_property_changed)
         RunButton.clicked.connect(self.run_function)
         RunButton2.clicked.connect(self.run_function2)
-        CompareResult_Button1.clicked.connect(self.select_compare_path1)
-        CompareResult_Button2.clicked.connect(self.select_compare_path2)
-        CompareResult_Button.clicked.connect(self.run_compare)
+        # CompareResult_Button1.clicked.connect(self.select_compare_path1)
+        # CompareResult_Button2.clicked.connect(self.select_compare_path2)
+        # CompareResult_Button.clicked.connect(self.run_compare)
 
         # 메인 윈도우 표시
         self.center_window(window)
@@ -163,9 +165,22 @@ class MyClass:
     def run_function(self):
         # 실행할 함수의 코드를 여기에 작성합니다.
         sFolderPath = self.datas.sFolderPath
+        def extract_datetime(folder_name):
+            try:
+                year = int(folder_name[:4])
+                month = int(folder_name[4:5])
+                day = int(folder_name[5:7])
+                hour = int(folder_name[7:9])
+                minute = int(folder_name[9:11])
+                second = int(folder_name[11:13])
+                return dt(year, month, day, hour, minute, second)  # Use the renamed datetime module
+            except ValueError:
+                # Handle incorrect folder name format gracefully
+                return dt.min
+    
         folder_list = [f for f in glob.glob(sFolderPath + "**/*", recursive=False) if os.path.isdir(f)]
-        folder_list.sort(key=os.path.getctime, reverse=False)
-
+        # folder_list.sort(key=os.path.getctime, reverse=False)
+        folder_list.sort(key=lambda folder: extract_datetime(os.path.basename(folder)))
         root_folder_path = r"D:\BlurScore_Review"
 
         # 폴더 생성
@@ -179,7 +194,12 @@ class MyClass:
         result_Arrays = []
         for folder in folder_list:
             xml_file = os.path.join(folder, "result.xml")
-            result_Array = []
+            result_Array = [[]]
+            result_Array.append([])
+            result_Array.append([])
+            result_Array.append([])
+            result_Array.append([])
+            result_Array.append([])
             # XML 파일이 존재하면
             if os.path.isfile(xml_file):
                 try:
@@ -194,16 +214,50 @@ class MyClass:
                                 print("Blur Count lower than 6")
                                 return
                             for blur in Blurs:
-                                result_Array.append(blur.get('BlurScore'))
-                    result_Arrays.append(result_Array)
+                                subIndex = blur.get('SubIndex')
+                                result_Array[int(subIndex) - 1].append(blur.get('BlurScore'))
+                    flattened_Array = []
+                    for inner_Array in result_Array:
+                        flattened_Array.extend(inner_Array)
+                    result_Arrays.append(flattened_Array)
                 except Exception as e:
                     print(f"Error parsing XML file: {xml_file}. Error: {e}")
             # result.xml이 없으면 skip
             else:
                 print(f"No result.xml file in folder: {folder}. Skipping to next folder.")
         save_path = os.path.join(work_folder_path, "result.png")
-        self.save_array_graph(result_Arrays, save_path, "BlurScore")
-        self.make_result_data(result_Arrays, work_folder_path)
+
+        converted_arrays = []
+        for arr in result_Arrays:
+            converted_arr = [float(element) for element in arr]
+            converted_arrays.append(converted_arr)
+        # converted_arrays = [np.array(sublist) for sublist in converted_arrays]
+        # 배열들을 NumPy 배열로 변환
+        data = np.array(converted_arrays)
+        # 각 열(요소별)의 평균과 표준편차 계산
+        means = np.mean(data, axis=0)
+        stddevs = np.std(data, axis=0)
+
+        sigma3_values = []
+        for i in range(len(means)):
+            lower_bound = means[i] - 3 * stddevs[i]
+            upper_bound = means[i] + 3 * stddevs[i]
+            sigma3_values.append((lower_bound + upper_bound) / 2)
+
+        variances = []
+        for i in range(len(means)):
+            variance = np.mean((data[:, i] - means[i]) ** 2)
+            variances.append(variance)
+
+        save_3sigma_path = os.path.join(work_folder_path, "3sigma.png")
+        chipNum = int(len(result_Arrays[0]) / 6)
+        self.make_result_data(result_Arrays, sigma3_values ,work_folder_path)
+        self.save_array_graph(result_Arrays, save_path, "BlurScore", chipNum)
+        # sigma3_values의 평균을 계산하여 출력
+        sigma3_mean = sum(sigma3_values) / len(sigma3_values)
+        variances_mean = sum(variances) / len(variances)
+        print("Sigma3 Values의 평균:", sigma3_mean)
+        print("variances Values의 평균:", variances_mean)
         print("Run Finished.")
 
 
@@ -219,56 +273,8 @@ class MyClass:
         if not os.path.exists(root_folder_path):
             os.makedirs(root_folder_path)
 
-        now = datetime.datetime.now()
-        work_folder_path = os.path.join(root_folder_path, now.strftime("%Y-%m-%d_%H-%M-%S"))
-        os.makedirs(work_folder_path)
 
-        result_Arrays = []
-        for folder in folder_list:
-            xml_file = os.path.join(folder, "result.xml")
-            result_Array = []
-            # XML 파일이 존재하면
-            if os.path.isfile(xml_file):
-                try:
-                    tree = ElementTree.parse(xml_file)
-                    root = tree.getroot()
-                    results = root.find(".//Results")
-                    if results is not None:
-                        for Feature in results.findall('.//Feature'):
-                            Index = Feature.get('Index')
-                            Blurs = Feature.findall('.//Blur')
-                            if len(Blurs) < 6:
-                                print("Blur Count lower than 6")
-                                return
-                            for blur in Blurs:
-                                result_Array.append(blur.get('BlurScore'))
-                    result_Arrays.append(result_Array)
-                except Exception as e:
-                    print(f"Error parsing XML file: {xml_file}. Error: {e}")
-            # result.xml이 없으면 skip
-            else:
-                print(f"No result.xml file in folder: {folder}. Skipping to next folder.")
-        save_path = os.path.join(work_folder_path, "variance_result.png")
-        result_Arrays_ToVariance = self.transform_to_variance_arrays(result_Arrays)
-        self.save_variance_graph(result_Arrays_ToVariance, save_path, "variance")
-        print("Run Finished.")
-    def transform_to_variance_arrays(self, input_arrays):
-        # 문자열을 숫자로 변환한 NumPy 배열 생성
-        numeric_arrays = np.array(input_arrays, dtype=float)
-        
-        # 열 단위로 분산 계산
-        variances = np.var(numeric_arrays, axis=0)
-        
-        # 결과 리스트 생성
-        result_Arrays_ToVariance = []
-        
-        # 결과 리스트에 분산 값과 각 열의 첫 번째 요소 추가
-        for col_idx, variance in enumerate(variances):
-            result_Arrays_ToVariance.append([variance])
-            
-        return result_Arrays_ToVariance
-
-    def make_result_data(self, result_Arrays, folder_path):
+    def make_result_data(self, result_Arrays, sigma_Arrays , folder_path):
         data = ElementTree.Element("data")
         count = ElementTree.SubElement(data, "count")
         count.text = str(len(result_Arrays))
@@ -292,124 +298,52 @@ class MyClass:
         plt.hist(data, bins='auto')
         plt.savefig(output_file)
         plt.close()
-    def save_array_graph(self, data_list, output_file, title):
-        plt.figure(figsize=(12.8, 9.6))  # 그래프 크기 조정
-        plt.title(title)
-        for data in data_list:
+
+    def save_array_graph(self, data_list, output_file, title, chipNum):
+        fig, ax = plt.subplots(figsize=(12.8, 9.6))
+        ax.set_title(title)
+        
+        color_map = cm.get_cmap('tab20b', len(data_list)//2)
+        color_map_c = cm.get_cmap('tab20c', len(data_list)//2)
+        
+        for idx, data in enumerate(data_list):
             temp = np.array(data).astype(float)
-            x = np.arange(len(temp))  # 인덱스를 x 축으로 사용
-            y = np.array(temp)  # 값들을 y 축으로 사용
-            plt.plot(x, y)  # 그래프 그리기
-
-        plt.xlabel("SubLine")
-        plt.ylabel("BlurScore")
-        plt.ylim(0, self.datas.nGraphHeight)
-        plt.yticks(np.arange(0, self.datas.nGraphHeight, self.datas.nYtick))  # y 축 눈금 설정
-        plt.legend([f'Data {i+1}' for i in range(0, len(data_list), 1)], bbox_to_anchor=(1.01, 1.15), loc='upper left')
-        plt.savefig(output_file)  # 그래프를 파일로 저장
-        plt.close()  # 그래프 창 닫기
-    def save_variance_graph(self, data_list, output_file, title):
-        plt.figure(figsize=(12.8, 9.6))  # 그래프 크기 조정
-        plt.title(title)
-        for data in data_list:
-            temp = np.array(data).astype(float)
-            x = np.arange(len(data_list))  # 인덱스를 x 축으로 사용
-            y = np.array(data_list)  # 값들을 y 축으로 사용
-            plt.plot(x, y)  # 그래프 그리기
-
-        plt.xlabel("SubLine")
-        plt.ylabel("BlurScore")
-        plt.ylim(0, self.datas.nGraphHeight)
-        plt.yticks(np.arange(0, self.datas.nGraphHeight, self.datas.nYtick))  # y 축 눈금 설정
-        plt.legend([f'Data {i+1}' for i in range(0, len(data_list), 1)], bbox_to_anchor=(1.01, 1.15), loc='upper left')
-        plt.savefig(output_file)  # 그래프를 파일로 저장
-        plt.close()  # 그래프 창 닫기
-
-    def save_two_array_graph(self, data1, data2, output_file):
-        x1 = np.arange(len(data1))  # 인덱스를 x 축으로 사용
-        y1 = np.array(data1)  # 값들을 y 축으로 사용
-        x2 = np.arange(len(data2))  # 인덱스를 x 축으로 사용
-        y2 = np.array(data2)  # 값들을 y 축으로 사용
-        plt.xlabel("Repeat")
-        plt.ylabel("Score")
-        plt.ylim(0, self.datas.nGraphHeight)
-        plt.plot(x1, y1, label='Data1')  # 그래프 1 그리기
-        plt.plot(x2, y2, label='Data2')  # 그래프 2 그리기
-        plt.legend()  # 범례 추가
-        plt.savefig(output_file)  # 그래프를 파일로 저장
-        plt.close()  # 그래프 창 닫기
-    def select_compare_path1(self):
-        dialog = QFileDialog()
-        dialog.setDirectory("D:\BlurScore_Review")
-        selected_path = dialog.getExistingDirectory(None, "Select Folder")
-        self.datas.sComparePath1 = selected_path
-        self.CompareResult_Path1.setText(selected_path) 
-    def select_compare_path2(self):
-        dialog = QFileDialog()
-        dialog.setDirectory("D:\BlurScore_Review")
-        selected_path = dialog.getExistingDirectory(None, "Select Folder")
-        self.datas.sComparePath2 = selected_path
-        self.CompareResult_Path2.setText(selected_path) 
-    def run_compare(self):
-        try:
-            first_xml_path = os.path.join(self.datas.sComparePath1, "data.xml")
-            second_xml_path = os.path.join(self.datas.sComparePath2, "data.xml")
-            first_Blur_Arrays = []
-            second_Blur_Arrays = []
-            message_box = QMessageBox()
-            if not os.path.isfile(first_xml_path) or not os.path.isfile(second_xml_path):
-                message_box.setWindowTitle("Warning")
-                message_box.setText("data.xml 파일이 없습니다")
-                message_box.exec_()
-                return
+            x = np.arange(len(temp))
+            y = np.array(temp)
+            
+            if idx % 2 == 0:
+                color = color_map(idx // 2)
             else:
-                first_tree = ElementTree.parse(first_xml_path)
-                first_data = first_tree.getroot()
-                first_lines = first_data.findall(".//Line")
-                for line in first_lines:
-                    # Find the 'Blurs' element
-                    blurs_element = line.find('.//Blurs')
-                    # Extract the Blur values
-                    blurs = []
-                    for blur_element in blurs_element.findall('Blur'):
-                        blur_value = float(blur_element.text)
-                        blurs.append(blur_value)
-                    first_Blur_Arrays.append(blurs)
-                second_tree = ElementTree.parse(second_xml_path)
-                second_data = second_tree.getroot()
-                second_lines = second_data.findall('.//Line')
-                for line in second_lines:
-                    # Find the 'Blurs' element
-                    blurs_element = line.find('.//Blurs')
-                    # Extract the Blur values
-                    blurs = []
-                    for blur_element in blurs_element.findall('Blur'):
-                        blur_value = float(blur_element.text)
-                        blurs.append(blur_value)
-                    second_Blur_Arrays.append(blurs)
-                if len(first_Blur_Arrays) != len(second_Blur_Arrays):
-                    message_box.setWindowTitle("Warning")
-                    message_box.setText("Line 갯수가 다릅니다")
-                    message_box.exec_()
-                    return
-        except Exception as e:
-            print(f"Error: {e}")
-        compare_folder_path = r"D:\BlurScore_Review\Compare"
-            # 폴더 생성
-        if not os.path.exists(compare_folder_path):
-            os.makedirs(compare_folder_path)
+                color = color_map_c((idx - 1) // 2)
+            
+            ax.plot(x, y, label=f'Data {idx+1}', color=color)
+            ax.text(x[-1] + 0.5, y[-1], str(idx+1), fontsize=8, ha='center', va='bottom')
 
-        now = datetime.datetime.now()
-        work_folder_path = os.path.join(compare_folder_path, now.strftime("%Y-%m-%d_%H-%M-%S"))
-        os.makedirs(work_folder_path)
-        tmp = 0
-        for ary1, ary2 in zip(first_Blur_Arrays, second_Blur_Arrays):
-            save_path = os.path.join(work_folder_path, f"plot{tmp+1}.png")
-            self.save_two_array_graph(ary1,ary2,save_path)
-            tmp+=1
-        print("Compare Done")
-
-
+        ax.set_xlabel("SubLine")
+        ax.set_ylabel("BlurScore")
+        ax.set_ylim(0, self.datas.nGraphHeight)
+        ax.set_xlim(0,len(data_list[0]))
+        pattern = ['1', '1', '1', '1', '1', '2', '2', '2', '2', '2' , '3', '3', '3', '3', '3' , '4', '4', '4', '4', '4' , '5', '5', '5', '5', '5' , '6', '6', '6', '6', '6']
+        ax.set_xticks(np.arange(0, len(data), 1), pattern)
+        ax.set_yticks(np.arange(0, self.datas.nGraphHeight, self.datas.nYtick))
+        ax.legend(bbox_to_anchor=(1.01, 1.15), loc='upper left')
+        
+        # Add a secondary x-axis (ax2)
+        ax2 = ax.twiny()  # Use twiny() instead of twinx() to create a new x-axis
+        
+        def ax2_to_x(x):
+            return x / 5  # Convert back to original scale
+        
+        x_positions = np.arange(-1, len(data_list[0]), 5)
+        ax2.set_xlim(ax.get_xlim())
+        ax2.set_xticks(x_positions)
+        ax2.set_xticklabels([f'{int(ax2_to_x(x)):g}' for x in x_positions])
+        ax2.set_xlabel("구분선")
+        for x_position in x_positions:
+            ax2.axvline(x=x_position, color='gray', linestyle='--', alpha=0.5)
+    
+        plt.savefig(output_file)
+        plt.close()
 
 # main.py 실행
 if __name__ == "__main__":
